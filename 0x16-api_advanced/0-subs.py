71fb1cdc0a07e5a4f8bb243bf0f3d83fc40a8183
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-05-08 17:25:35 +0000

    changesssss

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""
-Module to query the Reddit API and return the number of subscribers for a given subreddit.
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Function to retrieve the number of subscribers for a given subreddit.
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    Args:
-        subreddit (str): The name of the subreddit to query.
+    headers = {'User-Agent': 'RedditDataAnalyzer/1.0 (ALX Africa)'}
 
-    Returns:
-        int: The number of subscribers for the given subreddit, or 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+
+        subscribers = data.get('data', {}).get('subscribers', 0)
+
+        return subscribers
     else:
         return 0
-
-if __name__ == "__main__":
-    subreddit_name = input("Enter the name of the subreddit: ")
-    subscribers = number_of_subscribers(subreddit_name)
-    print(f"The number of subscribers in r/{subreddit_name} is: {subscribers}")
